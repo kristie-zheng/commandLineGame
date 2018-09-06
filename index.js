00976656cc741dd4be53f08ef7b0f292b7feff18
Kristie Desiree <kly5@mail.sfsu.edu>
2018-09-06 12:04:26 -0700

    Create functions that check if slot is occupied and by which piece

--- index.js
+++ index.js
@@ -3,4 +3,26 @@ const clear = require('clear');
 const figlet = require('figlet');
 const files = require('./lib/files');
 
-console.log("YOOOOO!")
+console.log("YOOOOO!");
+
+const gameboard = new Array(9);
+
+
+// function will return if a given index has a piece
+const isSlotOccupied = (index) => {
+  var isOccupied = false;
+  if (gameboard[index] !== undefined) {
+    isOccupied = true;
+  }
+  return isOccupied;
+};
+
+// function will return if slot is X or O 
+const slotOccupant = (index) => {
+  let occupant = "none";
+  if (gameboard[index] !== undefined) {
+    occupant = gameboard[index];
+  }
+  return occupant;
+};
+
