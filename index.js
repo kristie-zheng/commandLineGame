6f1ce79364c36337198d8b77df446955077ce83c
Kristie Desiree <kly5@mail.sfsu.edu>
2018-09-06 12:15:17 -0700

    Create function to check if board has winner

--- index.js
+++ index.js
@@ -26,3 +26,24 @@ const slotOccupant = (index) => {
   return occupant;
 };
 
+const winningCombinations = [
+  [1, 5, 9],
+  [3, 5, 7],
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [1, 4, 7],
+  [2, 5, 8],
+  [3, 6, 9],
+];
+
+const isWinner = () => {
+  let winner = false;
+  for (let i = 0; i < winningCombinations.length; i++) {
+    const combination = winningCombinations[i];
+    if (slotOccupant(combination[0]) === slotOccupant(combination[1]) === slotOccupant(combination[2]) && (slotOccupant(combination[0] !== 'none'))) {
+      winner = true;
+    }
+  }
+  return winner;
+};
